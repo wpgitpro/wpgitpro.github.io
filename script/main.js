666ebe314f167549931517c0fe8b475dadea2dab
Mark Rogers <mrogers@wpgitpro.onmicrosoft.com>
2018-01-02 22:36:18 -0600

    Reorg some files

--- script/main.js
+++ script/main.js
@@ -1,7 +1,11 @@
+/* sample function */
+
 function myTestGL() {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
-    ctx.moveTo(0,0);
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.moveTo(10,10);
     ctx.lineTo(200,100);
     ctx.stroke();
 }
