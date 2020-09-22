8980b379e254a7075b96c512378bf03fe83905a3
Mark Rogers <wpgitpro@outlook.com>
2020-09-22 09:11:31 -0500

    Formatting changes

--- script/main.js
+++ script/main.js
@@ -1,17 +1,1 @@
-var mycolours = ["blue","pink","coral","orange"];
-
-/* sample function */
-
-function myTestGL() {
-    var c = document.getElementById("myCanvas");
-    var ctx = c.getContext("2d");
-    ctx.lineWidth = 5;
-    ctx.lineCap = "round";
-    ctx.moveTo(10,10);
-    ctx.lineTo(200,100);
-    ctx.stroke();
-}
-
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min) ) + min;
-}
+var mycolours = ["lightcyan","pink","coral","orange"];
