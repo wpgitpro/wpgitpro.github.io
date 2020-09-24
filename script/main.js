c6bef6c6a98e4d62379d91382cf339e15b51a857
Mark Rogers <wpgitpro@outlook.com>
2020-09-24 10:06:19 -0500

    Fix background colours

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
- var mycolours = ["lightcyan","pink","coral","orange"];
+var mycolours = ["lightcyan","pink","coral","orange"];
 
- function getRndInteger(min, max) {
+function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 
- document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded',function() {
     document.querySelector(".wrapper").style.background = mycolours[getRndInteger(0,3)];
- });
+});
  
