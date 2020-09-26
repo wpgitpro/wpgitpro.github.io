2076b11bfe409cb88e0aae8462dbf9a5c4442631
Mark Rogers <wpgitpro@outlook.com>
2020-09-25 20:06:12 -0500

    Add an extra colour

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
-var mycolours = ["lavender","lightcyan","lemonchiffon","khaki"];
+var mycolours = ["lavender","lightcyan","lemonchiffon","khaki","peachpuff"];
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector(".wrapper").style.background = mycolours[getRndInteger(0,3)];
+    document.querySelector(".wrapper").style.background = mycolours[getRndInteger(0,4)];
 });
  
