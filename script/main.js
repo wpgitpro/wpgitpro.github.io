fb1bdd2dbbf58b45ba182136a3ad4f6f29554824
Mark Rogers <mrogers@wpgitpro.onmicrosoft.com>
2021-04-23 09:42:44 -0500

    Update colour selections

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
-var mycolours = ["lavender","lightcyan","lemonchiffon","khaki","peachpuff"];
+var mycolours = ["mintcream","bisque","lemonchiffon","khaki","peachpuff"];
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector(".wrapper").style.background = mycolours[getRndInteger(0,4)];
+    var newcolour = getRndInteger(0,mycolours.length-1);
+    document.querySelector(".wrapper").style.background = mycolours[newcolour];
+    document.querySelector("#sidebar").style.background = mycolours[newcolour];
 });
 
 document.addEventListener('DOMContentLoaded',function() {
