0e6e381d2313b074483464e8018858ea19eb07ca
Mark Rogers <mrogers@wpgitpro.onmicrosoft.com>
2021-04-27 11:23:03 -0500

    Colour change

--- script/main.js
+++ script/main.js
@@ -1,4 +1,6 @@
-var mycolours = ["mintcream","lavenderblush","lemonchiffon","khaki","peachpuff","navajowhite"];
+window.onload = init;
+
+var mycolours = ["mintcream","lavenderblush","mistyrose","papayawhip"];
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
@@ -8,6 +10,7 @@ document.addEventListener('DOMContentLoaded',function() {
     var newcolour = getRndInteger(0,mycolours.length-1);
     document.querySelector(".wrapper").style.background = mycolours[newcolour];
     document.querySelector("#sidebar").style.background = mycolours[newcolour];
+    document.querySelector("#footer").style.background = mycolours[newcolour];
 });
 
 document.addEventListener('DOMContentLoaded',function() {
@@ -15,4 +18,18 @@ document.addEventListener('DOMContentLoaded',function() {
         document.querySelector('#name').style.color = this.value;
     };
 });
+                
+function init() {
+    var sdate = getDateText();
+    var ele = document.getElementById("footer");
+    ele.innerHTML = ele.innerHTML + " - " + sdate;
+}
+
+function getDateText() {
+   var date = new Date();
+   var options = {
+      weekday: "long", year: "numeric", month: "short", day: "numeric"
+   };
+   return date.toLocaleDateString("en-CA", options);
+}
  
