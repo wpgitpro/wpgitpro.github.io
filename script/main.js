a7e9ee76e2e253acf92a801c28e3a86cd69cc803
Mark Rogers <wpgitpro@outlook.com>
2022-03-24 09:00:28 -0500

    First Update
    Update to start

--- script/main.js
+++ script/main.js
@@ -4,13 +4,12 @@ function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded', () => {
     document.querySelector(".wrapper").style.background = mycolours[getRndInteger(0,4)];
 });
 
 document.addEventListener('DOMContentLoaded',function() {
-    document.querySelector("select").onchange = function() {
-        document.querySelector('#name').style.color = this.value;
+    document.querySelector("h1").onpointerover = function() {
+        document.querySelector('.wrapper').style.background = mycolours[getRndInteger(0,4)];
     };
 });
- 
